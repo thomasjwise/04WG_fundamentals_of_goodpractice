28dfcd7aeca36d3f5f2da1c71305c71d056751ef
Wise <Tom.Wise@ons.gov.uk>
2026-05-11 15:38:36 +0100

    Base Documents

--- README.md
+++
@@ -1,2 +0,0 @@
-# 04WG_fundamentals_of_goodpractice
-Session 04 - Fundamentals of Good Practice in Coding
---
+++ worksheet_fundamentals_of_goodpractice.R
@@ -0,0 +1,10 @@
+# Fundamentals of Good Practice - Worksheet
+
+# Setup Execution 
+## This will use the function "source()" to run the separate R File "setup.R"
+## The "setup.R" file includes information such as: which packages to install, 
+## and to check the working directory is correctly set. 
+source("setup.R", local = FALSE)
+
+# Package Loading 
+library(tidyverse)
